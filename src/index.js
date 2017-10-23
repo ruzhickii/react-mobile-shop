da31f12dc4ad2895246ee7905924ecb0902b9913
ruzhickii <ruzhickiisasha@gmail.com>
2017-10-23 19:10:02 +0200

    create basket & modal phone page!

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-import './main.css';
+import './main.css'
 
-import React from 'react';
-import ReactDom from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import {browserHistory} from 'react-router';
-import {syncHistoryWithStore} from 'react-router-redux';
-import {Provider} from 'react-redux';
-import {Router, Route} from 'react-router';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {Provider} from 'react-redux'
 
-import reducers from 'reducers';
-import Layout from 'containers/layout';
-import Phones from 'containers/phones';
-import Phone from 'containers/phone';
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
+import Phone from 'containers/phone'
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
-));
+))
 
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(browserHistory, store)
 
-ReactDom.render(
+ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
             <Route component={Layout}>
                 <Route path='/' component={Phones} />
             </Route>
-            <Route path='phones/:id' component={Phone} />
+            <Route path='/phones/:id' component={Phone} />
         </Router>
     </Provider>,
     document.getElementById('root')
